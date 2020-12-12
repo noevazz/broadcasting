21f7da79953ca039be55d31d79f3c570882ab243
noe vazquez <jonovagu@gmail.com>
2020-12-12 15:04:51 -0600

    [ADD] adding an argument to specify the type (broadcast or phone)

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def main():
 
 @app.route('/sms', methods=['POST', 'GET'])
 def sms_reply():
-    
+    msg_type = request.form.get('msg_type', 'phone')
+    print(msg_type)
     
     # fetching incoming message
     #msg = request.form.get('Body')
@@ -25,23 +26,39 @@ def sms_reply():
     #resp.message("hola tu dijiste {}".format(msg))
 
     #return str(resp)
-    print(request.form.get('Body'))
-
+    #print(request.form.get('Body'))
     msg_from_post = request.form.get('msg')
-    to_phone_number = request.form.get('to_phone_number')
-    print(msg_from_post, to_phone_number)
-    message = client.messages.create(to=f'whatsapp:+{to_phone_number}', from_='whatsapp:+14155238886', body=msg_from_post ) 
- 
-    print(message.sid)
-    return message.sid
+
+    if msg_type == "phone":
+        to_phone_number = request.form.get('to_phone_number')
+        print(msg_from_post, to_phone_number)
+        message = client.messages.create(to=f'whatsapp:+{to_phone_number}', from_='whatsapp:+14155238886', body=msg_from_post ) 
+        print(message.sid)
+        return message.sid
+
+    elif msg_type=="broadcast":
+        to = []
+        for msgs in client.messages.list():
+            if("Join" in msgs.body):
+                if msgs.from_.replace('whatsapp:+','') not in to:
+                    to.append(msgs.from_.replace('whatsapp:+',''))
+                
+        print("#########################################################################")
+        print(to)
+        for number in to:
+            client.messages.create(to=f'whatsapp:+{number}', from_='whatsapp:+14155238886', body=msg_from_post )
+        return "Mensaje enviado a todos"
 
 @app.route('/users')
 def get_users():
     to = []
     for msgs in client.messages.list():
         if("Join" in msgs.body):
-            to.append(msgs.from_[10:])
-            print(msgs.from_)
+            if msgs.from_.replace('whatsapp:+','') not in to:
+                to.append(msgs.from_.replace('whatsapp:+',''))
+    
+    for number in to:
+        print(number)
     return jsonify(to)
 
 if __name__ == '__main__':
